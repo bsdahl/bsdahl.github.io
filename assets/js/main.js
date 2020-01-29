1d1ae77078f048a67549f4655ef3b9ff9125e01f
Ben Dahl <ben@foyyay.com>
2020-01-28 20:57:32 -0800

    A few updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,150 +20,151 @@
 */
 
 (function($) {
-    'use strict';
+  'use strict';
 
-    jQuery(document).ready(function() {
-
-
+  jQuery(document).ready(function() {
     /* XX. PRELOADER
     ==================================================*/
 
     $(window).on('load', function() {
-        $("#status").fadeOut();
-        $("#preloader").delay(500).fadeOut("slow");
-    })
-
+      $('#status').fadeOut();
+      $('#preloader')
+        .delay(500)
+        .fadeOut('slow');
+    });
 
     /* 01. HOME TEXT TYPING
     ==================================================*/
 
     $(window).on('load', function() {
-        $(".typing").typed({
-            strings: ["I am a Web Developer.", "I have 7 years experience.", "and I would like to work for you."],
-            typeSpeed: 70
-        });
+      var years = new Date().getFullYear() - 2010;
+      if (years < 10) years = 10;
+      $('.typing').typed({
+        strings: [
+          'I am a Web Developer.',
+          `I have ${years} years experience.`,
+          'and I would like to work for you.',
+        ],
+        typeSpeed: 70,
+      });
     });
-
 
     /* 02. ANIMATION EFFECT
     ==================================================*/
 
     AOS.init({
-        duration: 1200,
-        once: true,
-        disable: 'mobile'
+      duration: 1200,
+      once: true,
+      disable: 'mobile',
     });
-
 
     /* 03. PARALLAX BACKGROUND EFFECT
     ==================================================*/
 
     var parallax = function() {
-        $(window).stellar();
+      $(window).stellar();
     };
 
     $(function() {
-        parallax();
+      parallax();
     });
-
 
     /* 04. HOME ARROW DOWN
     ==================================================*/
 
     var arrowBounce = function() {
-        var arrow = $(".arrow");
-        if (arrow.hasClass("lift")) {
-            arrow.removeClass("lift");
-        } else {
-            arrow.addClass("lift");
-        }
+      var arrow = $('.arrow');
+      if (arrow.hasClass('lift')) {
+        arrow.removeClass('lift');
+      } else {
+        arrow.addClass('lift');
+      }
     };
 
     setInterval(arrowBounce, 800);
-
 
     /* 05. NAVIGATION
     ==================================================*/
 
     $(window).on('scroll', function() {
-        if ($(window).scrollTop() > 100) {
-            $('#navigation').addClass('navigation-background');
-        } else {
-            $('#navigation').removeClass('navigation-background');
-        }
+      if ($(window).scrollTop() > 100) {
+        $('#navigation').addClass('navigation-background');
+      } else {
+        $('#navigation').removeClass('navigation-background');
+      }
     });
-
 
     /* 06. MOBILE NAVIGATION
     ==================================================*/
 
     $(document).on('click', '.navbar-collapse.in', function(e) {
-        if ($(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle') {
-            $(this).collapse('hide');
-        }
+      if ($(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle') {
+        $(this).collapse('hide');
+      }
     });
 
     $('body').scrollspy({
-        target: '.navbar-collapse',
-        offset: 195
+      target: '.navbar-collapse',
+      offset: 195,
     });
-
 
     /* 07. SMOTH SCROLL
     ==================================================*/
 
     $('a.smoth-scroll').on('click', function(e) {
-        var anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $(anchor.attr('href')).offset().top - 50
-        }, 1000);
-        e.preventDefault();
+      var anchor = $(this);
+      $('html, body')
+        .stop()
+        .animate(
+          {
+            scrollTop: $(anchor.attr('href')).offset().top - 50,
+          },
+          1000
+        );
+      e.preventDefault();
     });
-
 
     /* 08. PORTFOLIO FILTER IMAGE
     ==================================================*/
     $('#port-image').mixItUp();
 
-
     /* 09. TESTIMONIAL SLIDER
     ==================================================*/
 
     $('.flexslider').flexslider({
-        animation: "slide"
+      animation: 'slide',
     });
-
 
     /* 10. CONTACT VALIDATION FORM
     ==================================================*/
 
     $(function() {
-        $('#contact-form').validate({
-            rules: {
-                name: {
-                    required: true,
-                    minlength: 2
-                },
-                email: {
-                    required: true
-                },
-                message: {
-                    required: true
-                }
-            },
-            messages: {
-                name: {
-                    required: "Please Input Your Name",
-                    minlength: "your name must consist of at least 2 characters"
-                },
-                email: {
-                    required: "Please Input Your Email"
-                },
-                message: {
-                    required: "Your Message Required"
-                }
-            },
-            /*submitHandler: function(form) {
+      $('#contact-form').validate({
+        rules: {
+          name: {
+            required: true,
+            minlength: 2,
+          },
+          email: {
+            required: true,
+          },
+          message: {
+            required: true,
+          },
+        },
+        messages: {
+          name: {
+            required: 'Please Input Your Name',
+            minlength: 'your name must consist of at least 2 characters',
+          },
+          email: {
+            required: 'Please Input Your Email',
+          },
+          message: {
+            required: 'Your Message Required',
+          },
+        },
+        /*submitHandler: function(form) {
                 $(form).ajaxSubmit({
                     type: "POST",
                     data: $(form).serialize(),
@@ -183,33 +184,32 @@
                     }
                 });
             }*/
-        });
+      });
     });
-
 
     /* 11. SCROLL ON TOP BUTTON
     ==================================================*/
 
     $(window).on('scroll', function() {
-        if ($(this).scrollTop() >= 500) {
-            $('.scroll-to-top').fadeIn();
-        } else {
-            $('.scroll-to-top').fadeOut();
-        }
+      if ($(this).scrollTop() >= 500) {
+        $('.scroll-to-top').fadeIn();
+      } else {
+        $('.scroll-to-top').fadeOut();
+      }
     });
 
     $('.scroll-to-top').on('click', function() {
-        $('html, body').animate({
-            scrollTop: 0
-        }, 800);
-        return false;
+      $('html, body').animate(
+        {
+          scrollTop: 0,
+        },
+        800
+      );
+      return false;
     });
 
     $(function() {
-        $('.service-item').matchHeight();
+      $('.service-item').matchHeight();
     });
-
-
   });
-
 })(jQuery);
--- index.md
+++ index.md
@@ -8,10 +8,10 @@ layout: home
 <hr class="line-title-left">
 #### Web Developer, Tech enthusiast in Ventura County California
 
-I am a man of many skills, but my passion is web development. My journey began in high school when I taught myself HTML and CSS. Shortly after, I landed my first job as a Webmaster at a two year Bible-college in Minnesota. I continued my work as a web developer both professionally and as a freelancer throughout college as I studied Computer Science for two years and then transferred to Liberty University to complete a *B.S. in Business Management Information Systems: Web Development.* I chose to study BMIS because while I loved computer science I felt that I would need more than programming skills to be successful in life. 
+I have many interests, but my passion is web development. My journey began in high school when I taught myself HTML and CSS. Shortly after, I landed my first job as a Webmaster at a Bible-college in Minnesota. I continued my work as a web developer both professionally and as a freelancer throughout college as I studied Computer Science for two years and then transferred to Liberty University to complete a *B.S. in Business Management Information Systems: Web Development.* I chose to study BMIS because while I loved computer science I felt that I would need more than programming skills to be successful in life. 
 
 Aside from programming in and web development, I've also worked in I.T. and Radio Broadcasting. I have a General Class Ham Radio license and enjoy working with DIY electronics. Occasionally, I design and build small business networks including enterprise wireless, routing, switching, and even pulling cable. 
 
-I believe I am different than most people in my field, because I love both technology and I love people. People say that they enjoy working with me and that I am a good teacher. Having just completed my Bachelors degree in May of 2017, I am looking forward to finding a full-time job as a Web developer and working on meaningful applications.
+I believe I am different than most people in my field, because I love both technology and I love people. People say that they enjoy working with me and that I am a good teacher.
 
 <a href="{{ "assets/benjamindahl.pdf" | relative_url }}" target="_blank"><button type="button" class="btn btn-success btn-lg">Download my Resume</button></a>
